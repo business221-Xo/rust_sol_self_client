7296ee8ed4578c33d6f97a0331437c7f75dedce2
business221-Xo <auction221business@gmail.com>
2025-01-24 03:25:08 -0500

    update account

--- solanadoc/anchor_crud_message/programs/anchor_crud_message/src/lib.rs
+++ solanadoc/anchor_crud_message/programs/anchor_crud_message/src/lib.rs
@@ -6,10 +6,10 @@ declare_id!("J4benTfF2csTh4GJcjrhC7zboKk8UyL8gc6B9RMGWD2H");
 pub mod anchor_crud_message {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
-    pub fn creat(_ctx : Context<Creat>) -> Result<()> {
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     Ok(())
+    // }
+    pub fn creat(_ctx : Context<Create>) -> Result<()> {
         Ok(())
     }
     pub fn update(_ctx : Context<Update>) -> Result<()> {
@@ -21,5 +21,23 @@ pub mod anchor_crud_message {
     
 }
 
+// #[derive(Accounts)]
+// pub struct Initialize {}
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create {
+
+}
+#[derive(Accounts)]
+pub struct Update{
+
+}
+#[derive(Accounts)]
+pub struct Delete {
+
+}
+#[account]
+pub struct MessageAccount{
+    pub user : Pubkey,
+    pub message : String,
+    pub bump : u8,
+}
